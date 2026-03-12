4f161113a3ec525ea6c433abe5e865b091bec0fa
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-12 14:40:31 +0000

    fix: update relations in ChargingSession model

--- app/Models/ChargingSession.php
+++ app/Models/ChargingSession.php
@@ -9,11 +9,13 @@ class ChargingSession extends Model
     protected $table = 'charging_sessions';
 
     protected $fillable = [
+
         'reservation_id',
         'start_time',
         'end_time',
         'energy_delivered_kwh',
         'total_cost'
+        
     ];
     
     public function reservation()
